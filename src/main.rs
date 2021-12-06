4e79be4483f119da5a67a2e8f486263c66580162
Dillion Nason <nasond432@gmail.com>
2021-12-05 21:43:31 -0500

    Cube render with perspective and view matrices

---
+++ src/main.rs
@@ -0,0 +1,202 @@
+extern crate image;
+
+mod teapot;
+
+use glium::{
+    glutin::{
+        event,
+        event::{Event, WindowEvent, DeviceEvent, VirtualKeyCode, KeyboardInput},
+        event_loop::{EventLoop, ControlFlow},  
+        window::WindowBuilder, 
+        dpi::LogicalSize, 
+        ContextBuilder
+    }, 
+    Surface,
+    Display, 
+    implement_vertex, 
+    uniform,
+    VertexBuffer, 
+    index::PrimitiveType, 
+    Program, 
+    IndexBuffer, DrawParameters, draw_parameters
+};
+
+fn main() {
+    // initialize our event_loop, window, OpenGL context, and display
+    let mut events_loop = EventLoop::new();
+    let wb = WindowBuilder::new()
+        .with_inner_size(LogicalSize::new(1920, 1080))
+        .with_title("Ferrous OpenGL");
+    let cb = ContextBuilder::new().with_depth_buffer(24);
+    let display = Display::new(wb, cb, &events_loop).unwrap();
+
+    // begin the event loop to keep the window open
+    event_loop(events_loop, display);
+}
+
+fn event_loop(event_loop: EventLoop<()>, display: Display) {
+    let positions = VertexBuffer::new(&display, &teapot::VERTICES).unwrap();
+    let mut t: f32 = -0.5;
+    let normals = VertexBuffer::new(&display, &teapot::NORMALS).unwrap();
+    let indices = IndexBuffer::new(&display, PrimitiveType::TrianglesList, &teapot::INDICES).unwrap(); 
+
+
+    let vertex_shader_src = r#"
+        #version 150
+
+        in vec3 position;
+        in vec3 normal;
+
+        out vec3 v_normal;
+
+        uniform mat4 perspective;
+        uniform mat4 view;
+        uniform mat4 model;
+
+        void main() {
+            mat4 modelview = view * model;
+            v_normal = transpose(inverse(mat3(modelview))) * normal;
+            gl_Position = perspective * modelview * vec4(position, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+
+        in vec3 v_normal;
+        out vec4 color;
+        uniform vec3 u_light;
+
+        void main() {
+            float brightness = dot(normalize(v_normal), normalize(u_light));
+            vec3 dark_color = vec3(0.6, 0.0, 0.0);
+            vec3 regular_color = vec3(1.0, 0.0, 0.0);
+            color = vec4(mix(dark_color, regular_color, brightness), 1.0);
+        }
+    "#;
+
+    let program = Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
+    event_loop.run(move |ev, _, control_flow| {
+        match ev {
+            Event::DeviceEvent { event, .. } => {
+                match event {
+                    event::DeviceEvent::Key(KeyboardInput { virtual_keycode, .. }) => {
+                        match virtual_keycode.unwrap() {
+                            VirtualKeyCode::Escape => {
+                                *control_flow = ControlFlow::Exit;
+                                return;
+                            },
+                            _ => return,
+                        }
+                    },
+                    _ => return,
+                }
+            },
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => {
+                    *control_flow = ControlFlow::Exit;
+                    return;
+                },
+                _ => return,
+            },
+            Event::NewEvents(cause) => match cause {
+                event::StartCause::ResumeTimeReached { .. } => (),
+                event::StartCause::Init => (),
+                _ => return,
+            },
+            _ => (),
+        }
+
+        let next_frame_time = std::time::Instant::now() +
+            std::time::Duration::from_nanos(16_666_667);
+        *control_flow = ControlFlow::WaitUntil(next_frame_time);
+
+        t += 0.0002;
+        if t > 0.5 {
+            t = -0.5;
+        }
+
+        // draw triangle to the window
+        let mut target = display.draw();
+        target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
+
+        let perspective = {
+            let (width, height) = target.get_dimensions();
+            let aspect_ratio = height as f32 / width as f32;
+            
+            let fov: f32 = 3.141592 / 3.0;
+            let zfar = 1024.0;
+            let znear = 0.1;
+
+            let f = 1.0 / (fov / 2.0).tan();
+
+            [
+                [ f * aspect_ratio  , 0.0,              0.0              ,  0.0 ],
+                [       0.0         ,  f ,              0.0              ,  0.0 ],
+                [       0.0         , 0.0,  (zfar+znear)/(zfar-znear)    ,  1.0 ],
+                [       0.0         , 0.0, -(2.0*zfar*znear)/(zfar-znear),  0.0 ],
+            ]
+        };
+
+        let model = [
+            [ 0.01, 0.0, 0.0, 0.0 ],
+            [ 0.0, 0.01, 0.0, 0.0 ],
+            [ 0.0, 0.0, 0.01, 0.0 ],
+            [ 0.0, 0.0, 2.0, 1.0f32 ]
+        ];
+
+        let params = DrawParameters {
+            depth: glium::Depth { 
+                test: draw_parameters::DepthTest::IfLess, 
+                write: true,
+                .. Default::default() 
+            },
+            // enables backface culling, not wanted on the pot
+            //backface_culling: draw_parameters::BackfaceCullingMode::CullClockwise,
+            .. Default::default()
+        };
+
+        let light = [-0.1, 0.4, 0.9f32];
+
+        let view = view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]);
+
+        target.draw((&positions, &normals), &indices, &program, 
+            &uniform! { model: model, view: view, perspective: perspective, u_light: light }, &params).unwrap();
+        target.finish().unwrap();
+    });
+}
+
+fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
+    let f = {
+        let f = direction;
+        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
+        let len = len.sqrt();
+        [f[0] / len, f[1] / len, f[2] / len]
+    };
+
+    let s = [up[1] * f[2] - up[2] * f[1],
+             up[2] * f[0] - up[0] * f[2],
+             up[0] * f[1] - up[1] * f[0]];
+
+    let s_norm = {
+        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
+        let len = len.sqrt();
+        [s[0] / len, s[1] / len, s[2] / len]
+    };
+
+    let u = [f[1] * s_norm[2] - f[2] * s_norm[1],
+             f[2] * s_norm[0] - f[0] * s_norm[2],
+             f[0] * s_norm[1] - f[1] * s_norm[0]];
+
+    let p = [-position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
+             -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
+             -position[0] * f[0] - position[1] * f[1] - position[2] * f[2]];
+
+    [
+        [s_norm[0], u[0], f[0], 0.0],
+        [s_norm[1], u[1], f[1], 0.0],
+        [s_norm[2], u[2], f[2], 0.0],
+        [p[0], p[1], p[2], 1.0],
+    ]
+}
